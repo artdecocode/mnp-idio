086c711652aee9eef43c08bb4f4db192caa736e5
Anton <anton@adc.sh>
2018-05-02 03:24:10 +0100

    _yarn.unlock

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path')
 const { createReadStream, createWriteStream } = require('fs')
 
-const from = resolve(__dirname, 'structure/_yarn.lock')
+const from = resolve(__dirname, 'structure/_yarn.unlock')
 const to = resolve(__dirname, 'structure/yarn.lock')
 
 const rs = createReadStream(from)
--- postpublish.js
+++ postpublish.js
@@ -1,5 +1,7 @@
 const { resolve } = require('path')
 const { unlink } = require('fs')
 
-const file = resolve(__dirname, 'structure/_yarn.lock')
-unlink(file, () => {})
+const file = resolve(__dirname, 'structure/_yarn.unlock')
+unlink(file, () => {
+  console.log('removed file _yarn.unlock')
+})
